c02fb40b0ad0db2e0921912da4041d9989406952
User <mariatameeva5@gmail.com>
2021-08-31 15:10:21 +0300

    version

--- gamezmeika.py
+++ gamezmeika.py
@@ -5,6 +5,7 @@ import pygame_menu
 RES = 800
 SIZE = 50
 
+
 x, y = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
 apple = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
 length = 1
@@ -21,78 +22,77 @@ font_score = pygame.font.SysFont('Arial', 26, bold=True)
 font_end = pygame.font.SysFont('Arial', 66, bold=True)
 img = pygame.image.load('reg.jpg').convert()
 
+
+
+
 def close_game():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exit()
+                exit()
 
-# while True:
-#     surface.blit(img, (0, 0))
-#     # drawing snake, apple
-#     [pygame.draw.rect(surface, pygame.Color('green'), (i, j, SIZE - 1, SIZE - 1)) for i, j in snake]
-#     pygame.draw.rect(surface, pygame.Color('red'), (*apple, SIZE, SIZE))
-#     # show score
-#     render_score = font_score.render(f'SCORE: {score}', 1, pygame.Color('orange'))
-#     surface.blit(render_score, (5, 5))
-#     # snake movement
-#     x += dx * SIZE
-#     y += dy * SIZE
-#     snake.append((x, y))
-#     snake = snake[-length:]
-#     # eating food
-#     if snake[-1] == apple:
-#         apple = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
-#         length += 1
-#         fps += 1
-#         score += 1
-#     # game over
-#     if x < 0 or x > RES - SIZE or y < 0 or y > RES - SIZE or len(snake) != len(set(snake)):
-#         while True:
-#             render_end = font_end.render('GAME OVER', 1, pygame.Color('orange'))
-#             surface.blit(render_end, (RES // 2 - 200, RES // 3))
-#             pygame.display.flip()
-#             close_game()
-#
-#     pygame.display.flip()
-#     clock.tick(fps)
-#     close_game()
-#     # controls
-#     key = pygame.key.get_pressed()
-#     if key[pygame.K_w]:
-#         if dirs['W']:
-#             dx, dy = 0, -1
-#             dirs = {'W': True, 'S': False, 'A': True, 'D': True, }
-#     if key[pygame.K_s]:
-#         if dirs['S']:
-#             dx, dy = 0, 1
-#             dirs = {'W': False, 'S': True, 'A': True, 'D': True, }
-#     if key[pygame.K_a]:
-#         if dirs['A']:
-#             dx, dy = -1, 0
-#             dirs = {'W': True, 'S': True, 'A': True, 'D': False, }
-#     if key[pygame.K_d]:
-#         if dirs['D']:
-#             dx, dy = 1, 0
-#             dirs = {'W': True, 'S': True, 'A': False, 'D': True, }
-#
+while True:
+    surface.blit(img, (0, 0))
+    # drawing snake, apple
+    [pygame.draw.rect(surface, pygame.Color('green'), (i, j, SIZE - 1, SIZE - 1)) for i, j in snake]
+    pygame.draw.rect(surface, pygame.Color('red'), (*apple, SIZE, SIZE))
+    # show score
+    render_score = font_score.render(f'SCORE: {score}', 1, pygame.Color('orange'))
+    surface.blit(render_score, (5, 5))
+    # snake movement
+    x += dx * SIZE
+    y += dy * SIZE
+    snake.append((x, y))
+    snake = snake[-length:]
+    # eating food
+    if snake[-1] == apple:
+        apple = randrange(SIZE, RES - SIZE, SIZE), randrange(SIZE, RES - SIZE, SIZE)
+        length += 1
+        fps += 1
+        score += 1
+    # game over
+    if x < 0 or x > RES - SIZE or y < 0 or y > RES - SIZE or len(snake) != len(set(snake)):
 
 
+        while True:
+            render_end = font_end.render('GAE OVER', 1, pygame.Color('orange'))
+            surface.blit(render_end, (RES // 2 - 200, RES // 3))
+            pygame.display.flip()
+            close_game()
 
-def start_the_game():
-    # Do the job here !
-    pass
+    pygame.display.flip()
+    clock.tick(fps)
+    close_game()
+    # controls
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        if dirs['W']:
+            dx, dy = 0, -1
+            dirs = {'W': True, 'S': False, 'A': True, 'D': True, }
+    if key[pygame.K_s]:
+        if dirs['S']:
+            dx, dy = 0, 1
+            dirs = {'W': False, 'S': True, 'A': True, 'D': True, }
+    if key[pygame.K_a]:
+        if dirs['A']:
+            dx, dy = -1, 0
+            dirs = {'W': True, 'S': True, 'A': True, 'D': False, }
+    if key[pygame.K_d]:
+        if dirs['D']:
+            dx, dy = 1, 0
+            dirs = {'W': True, 'S': True, 'A': False, 'D': True, }
+
+
+
+
+
+
 
 menu = pygame_menu.Menu('Welcome', 400, 300,
-                       theme=pygame_menu.themes.THEME_BLUE)
+theme=pygame_menu.themes.THEME_BLUE)
 
-menu.add.text_input('Имя :', default='Игрок 1')
-menu.add.button('Играть', start_the_game)
-menu.add.button('Выход', pygame_menu.events.EXIT)
-
+menu.add.text_input('Name :', default='John Doe')
+menu.add.button('Play', start_the_game)
+menu.add.button('Quit', pygame_menu.events.EXIT)
 
 menu.mainloop(surface)
-
-
-
-
 
